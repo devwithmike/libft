7ee4a13897a6a6b33ecc83e3cfdf421c33e901db
mike <mimeyer@student.wethinkcode.com>
2019-05-19 22:36:23 +0200

    adding even more functions

--- includes/libft.h
+++ includes/libft.h
@@ -46,5 +46,7 @@ char	*ft_strnew(size_t size);
 void	ft_memdel(void **ap);
 void	ft_strdel(char **as);
 void	ft_strclr(char *s);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 #endif
---
+++ tests/ft_striteri_test.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri_test.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mimeyer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/19 22:34:01 by mimeyer           #+#    #+#             */
+/*   Updated: 2019/05/19 22:34:13 by mimeyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void    ft_striteri(char *s, void (*f)(unsigned int, char *))                    
+{                                                                               
+     unsigned int i;                                                             
+                                                                                 
+     i = 0;                                                                      
+     if (s && f)                                                                 
+         while (*s)                                                              
+             f(i++, s++);                                                        
+}  
+
+void do_something(unsigned int i, char *s)
+{
+  printf("i: '%d'  s: '%s'\n",i, s);
+}
+
+int main()
+{
+  char test[] = "Hello World.";
+  ft_striteri(test, &do_something);
+  /* done */
+  return 0;
+}
