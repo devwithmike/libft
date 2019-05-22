ee2b1bd10f0cb2b2a38bef4de73ec55c2a272c20
Michael MEYER <mimeyer@c4r21s3.wethinkcode.co.za>
2019-05-22 15:26:37 +0200

    sdakj

--- src/ft_strrchr.c
+++ src/ft_strrchr.c
@@ -6,22 +6,22 @@
 /*   By: mimeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 11:57:12 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/05/21 09:53:12 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/05/22 15:22:42 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../includes/libft.h"
 
 char	*ft_strrchr(const char *str, int c)
 {
 	int i;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
+	i = ft_strlen(str);
 	while (i > 0)
 	{
 		if (str[i] == c)
 			return (&(*((char *)str + i)));
 		i--;
 	}
-	return (0);
+	return (NULL);
 }
