22f960d2fd6a9ec26b35088136db3db7e9a7ce22
Michael MEYER <mimeyer@c4r21s3.wethinkcode.co.za>
2019-05-23 11:51:45 +0200

    ahdsjhsdk

--- src/ft_putnbr_fd.c
+++ src/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mimeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 14:47:53 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/05/23 11:41:33 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/05/23 11:51:06 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
+	{
 		ft_putstr_fd("-2147483648", fd);
 		return ;
+	}
 	if (n < 0)
 	{
-		n *= -1;
 		ft_putchar_fd('-', fd);
+		n *= -1;
 	}
 	if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd(n % 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
 	if (n < 10)
 		ft_putchar_fd(n + 48, fd);
