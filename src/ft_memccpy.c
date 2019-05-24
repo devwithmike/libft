0cd44079bd9b4147c719bb4e6d46b6c7c90b0302
Michael MEYER <mimeyer@c4r21s3.wethinkcode.co.za>
2019-05-24 08:49:17 +0200

    memccpy lives

--- src/ft_memccpy.c
+++ src/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: mimeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 11:51:22 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/05/23 08:35:52 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/05/24 08:47:54 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,19 @@
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	unsigned int	i;
 	unsigned char	*dest2;
 	unsigned char	*src2;
 
 	dest2 = (unsigned char *)dest;
 	src2 = (unsigned char *)src;
-	i = 0;
-	while (i < n)
+	while (n != 0)
 	{
-		*((char *)dest2 + i) = *((char *)src2 + i);
-		i++;
-		if (*((char *)src2 + i) == ((unsigned char)c))
-		{
-			*((char *)dest2 + i) = *((char *)src2 + i);
-			break ;
-		}
+		*dest2 = *src2;
+		if (*src2 == (unsigned char)c)
+			return (dest2 + 1);
+		dest2++;
+		src2++;
+		n--;
 	}
-	return (dest2);
+	return (NULL);
 }
